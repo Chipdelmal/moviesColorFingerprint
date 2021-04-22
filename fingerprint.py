b896663a1e7b32d042af59354287ec3302170248
chipdelmal <chipdelmal@gmail.com>
2021-04-22 13:45:25 -0700

    Further improvements on SH pipe

--- fingerprint.py
+++ fingerprint.py
@@ -9,23 +9,26 @@ import numpy as np
 import auxiliary as aux
 # User inputs -----------------------------------------------------------------
 if aux.isNotebook():
-    (FILE, DOMINANT, FRAMES, DPI) = ("Gattaca", 1, 1000, 1000)
-    (IN_PATH, OUT_PATH) = (
+    (FILE, DOMINANT, FRAMES, DPI) = ("PrincessMononoke", 1, 3600, 1000)
+    (IN_PATH, OUT_PATH, TITLE) = (
         "/mnt/Luma/Videos/Movies/Frames", 
-        '/mnt/Luma/Videos/Movies/'
+        '/mnt/Luma/Videos/Movies/',
+        'Princess\nMononoke'
     )
 else:
     (FILE, DOMINANT, FRAMES, DPI) = (
         sys.argv[1], int(sys.argv[2]), 
         int(sys.argv[3]), int(sys.argv[4])
     )
-    (IN_PATH, OUT_PATH) = (sys.argv[5], sys.argv[6])
+    (IN_PATH, OUT_PATH, TITLE) = (
+        sys.argv[5], sys.argv[6], sys.argv[7]
+    )
 # Get frames paths and calculate the dominant clusters of the images ----------
 filepaths = aux.getFilepaths(IN_PATH, FILE)
 clusters = aux.calculateDominantColors(filepaths, DOMINANT)
 # Export the resulting fingerprints -------------------------------------------
 aux.exportFingerprintPlot(
     OUT_PATH, FILE+'.png', clusters, dpi=DPI, 
-    aspect=FRAMES/DOMINANT, movieTitle=FILE, fontsize=125, 
-    fontfamily='Gotham Light', color='#FFFFFF68'
+    aspect=FRAMES/DOMINANT, movieTitle=str(TITLE), fontsize=100, 
+    fontfamily='Gotham Light', color='#FFFFFF88'
 )
--- fingerprint.sh
+++ fingerprint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-FNAME='Interstellar.mp4'
+TITLE='Princess\nMononoke'
+FNAME="PrincessMononoke.mp4"
 # Constants that shouldn't be constants ---------------------------------------
 DOM='1'
 SCALE='384:160'
-FRNUM='1000'
+FRNUM='3600'
 DPI='1000'
 PT_I='/mnt/Luma/Videos/Movies'
 # Internally-generated scratch folders ----------------------------------------
@@ -18,12 +19,12 @@ mkdir -p $PT_O
 # Rescale movie ---------------------------------------------------------------
 echo "Processing ${FNAME%.*}"
 echo "[1/3] Re-scaling movie..."
-ffmpeg -loglevel panic -i "$PT_I/$FNAME" -vf "scale=$SCALE" "$PT_R/$FNAME"
+# ffmpeg -loglevel panic -i "$PT_I/$FNAME" -vf "scale=$SCALE" "$PT_R/$FNAME"
 echo "[2/3] Exporting frames..."
-python exportFrames.py $FNAME $FRNUM $PT_R $PT_F
+# python exportFrames.py $FNAME $FRNUM $PT_R $PT_F
 echo "[3/3] Generating fingerprint..."
-python fingerprint.py "${FNAME%.*}" $DOM $FRNUM $DPI $PT_F $PT_O
-echo "Done!"
+python fingerprint.py "${FNAME%.*}" $DOM $FRNUM $DPI $PT_F $PT_O $TITLE
+echo "Processing done!"
 # Delete scratch folders  -----------------------------------------------------
 # rm -r $PT_R
 # rm -r $PT_F
