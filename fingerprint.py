e78542a6b59f55d6e6b257b16404d0b69c22c6a6
chipdelmal <chipdelmal@gmail.com>
2021-11-05 11:09:03 -0700

    Colors are now dumped to pkl files

--- fingerprint.py
+++ fingerprint.py
@@ -9,18 +9,19 @@ import sys
 from os import path
 import multiprocessing
 import auxiliary as aux
+import pickle as pkl
 
 JOBS = multiprocessing.cpu_count()
 # User inputs -----------------------------------------------------------------
 if aux.isNotebook():
     # For testing and debugging in jupyter ------------------------------------
     (FILE, DOMINANT, CLUSTERS, FRAMES, DPI) = (
-        "1937_SnowWhite", 5, 10, 3600, 1000
+        "1937_SnowWhite", 2, 10, 3600, 1000
     )
     (IN_PATH, OUT_PATH, TITLE) = (
         "/mnt/Luma/Videos/Movies/Frames", 
-        "/mnt/Luma/Pictures/Art/Movies/",
-        "Moon"
+        "/mnt/Luma/Pictures/Art/Movies/Disney",
+        "Snow White"
     )
 else:
     # For calls from the terminal ---------------------------------------------
@@ -40,10 +41,13 @@ clusters = aux.parallelDominantImage(
 )
 # clusters = aux.calculateDominantColors(filepaths, DOMINANT, CLUSTERS)
 # Export the resulting fingerprints -------------------------------------------
-aux.exportFingerprintPlot(
-    OUT_PATH, FILE+'.png', clusters, dpi=DPI, 
-    aspect=FRAMES/DOMINANT, movieTitle=str(TITLE).format(), 
-    fontsize=30, fontfamily='Gotham XLight', 
-    color='#FFFFFFFF', textpos=(0.5, (.5-.35)/2),
-    facecolor='#000000FF', hspan=(.35, .5)
-)
+# aux.exportFingerprintPlot(
+#     OUT_PATH, FILE+'.png', clusters, dpi=DPI, 
+#     aspect=FRAMES/DOMINANT, movieTitle=str(TITLE).format(), 
+#     fontsize=30, fontfamily='Gotham XLight', 
+#     color='#FFFFFFFF', textpos=(0.5, (.5-.35)/2),
+#     facecolor='#000000FF', hspan=(.35, .5)
+# )
+# Export colorfiles -----------------------------------------------------------
+with open(path.join(OUT_PATH, FILE+'.pkl'),'wb') as file:
+    pkl.dump(clusters, file)
--- fingerprint.sh
+++ fingerprint.sh
@@ -20,9 +20,9 @@ mkdir -p $PT_O
 # Rescale movie ---------------------------------------------------------------
 echo "* Processing: $FNAME"
 echo "[1/3] Re-scaling $FNAME..."
-ffmpeg -n -loglevel panic -i "$PT_I/$FNAME" -vf "scale=$SCALE" "$PT_R/$FNAME"
+# ffmpeg -n -loglevel panic -i "$PT_I/$FNAME" -vf "scale=$SCALE" "$PT_R/$FNAME"
 echo "[2/3] Exporting frames..."
-python exportFrames.py $FNAME $FRNUM $PT_R $PT_F
+# python exportFrames.py $FNAME $FRNUM $PT_R $PT_F
 echo "[3/3] Generating fingerprint..."
 python fingerprint.py "${FNAME%.*}" $DOM $CLS $FRNUM $DPI $PT_F $PT_O "$TITLE"
 # Delete scratch folders  -----------------------------------------------------
