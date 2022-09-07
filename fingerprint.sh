8a5dd8fbd93f2237f648265774ff1322d73cfd49
chipdelmal <chipdelmal@gmail.com>
2022-09-07 08:48:55 -0700

    Minor improvements on bash scripts

--- fingerprint.sh
+++ fingerprint.sh
@@ -4,30 +4,44 @@ PT_I=$1
 PT_O=$2
 FNAME=$3 # "Nausicaa.mp4"
 TITLE=$4 # "Nausicaä\nof the\nValley\nof the\nWind"
-# Constants that shouldn't be constants ---------------------------------------
+###############################################################################
+# Constants that shouldn't be constants
+###############################################################################
 DOM='1'
 CLS='5'
 SCALE='640:370' # '480:270'
 FRNUM='3600'
 DPI='1000'
-# Internally-generated scratch folders ----------------------------------------
+###############################################################################
+# Internally-generated scratch folders
+###############################################################################
 PT_R="$PT_I/Rescaled"
 PT_F="$PT_I/Frames"
-# Create directories ----------------------------------------------------------
+###############################################################################
+# Create directories
+###############################################################################
 mkdir -p $PT_R
 mkdir -p $PT_F
 mkdir -p $PT_O
-
-# Rescale movie ---------------------------------------------------------------
-echo "* Processing: $FNAME"
-echo "[1/4] Re-scaling $FNAME..."
+###############################################################################
+# Print colors
+###############################################################################
+LGREEN='\033[1;36m'
+NC='\033[0m'
+###############################################################################
+# Rescale movie
+###############################################################################
+printf "${LGREEN}* Processing: $FNAME ${NC}\n"
+printf "\t [1/3] Re-scaling $FNAME..."
 ffmpeg -n -loglevel panic -i "$PT_I/$FNAME" -vf "scale=$SCALE" "$PT_R/$FNAME"
-echo "[2/4] Exporting frames..."
+printf "\t [2/3] Exporting frames..."
 python exportFrames.py $FNAME $FRNUM $PT_R $PT_F
-echo "[3/4] Generating fingerprint..."
+printf "\t [3/3] Generating fingerprint..."
 python fingerprint.py "${FNAME%.*}" $DOM $CLS $FRNUM $DPI $PT_F $PT_O "$TITLE"
-echo "[4/4] Generating colorspace plots..."
-python plotColor.py "${FNAME%.*}" $DOM $CLS $FRNUM $DPI $PT_F $PT_O "$TITLE"
-# Delete scratch folders  -----------------------------------------------------
+# echo "[4/4] Generating colorspace plots..."
+# python plotColor.py "${FNAME%.*}" $DOM $CLS $FRNUM $DPI $PT_F $PT_O "$TITLE"
+###############################################################################
+# Delete scratch folders
+###############################################################################
 # rm -r $PT_R
 # rm -r $PT_F
